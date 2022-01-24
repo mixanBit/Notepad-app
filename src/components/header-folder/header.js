bc7c4e0f28228a6bab7c0d3b1cae69eaaff8375d
mixanBit <65313661+mixanBit@users.noreply.github.com>
2022-01-24 16:16:47 +0300

    Google Authenticator

--- src/components/header-folder/header.js
+++ src/components/header-folder/header.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import './header.css'
 
+import GoogleLogin, { GoogleLogout } from 'react-google-login';
+
+import { useGoogleLogin } from 'react-google-login'
+// import { useGoogleLogout } from 'react-google-login'
+
+
+
+
 
 export class header extends React.Component {
+
+  
+  resposeGoogle=(response)=>{
+    console.log(response);
+    console.log(response.profileObj);
+    let name = response.profileObj.name;
+  }
+
+  logout=()=>{
+    console.log('выход из аккаунта');
+  }
+
   render() {
     return (
       <header className='header'>
@@ -19,11 +39,33 @@ export class header extends React.Component {
           </div>
 
           <div className='settings'>
-            <img className='settings_icon' src="./images/setting.png" alt="settings" />
+            <img className='settings_icon' src='./images/setting.png' alt="settings" />
+
+            <GoogleLogin 
+              clientId='101084119038-ro92b6i4fevieqo57pr9tk136pfsnfo6.apps.googleusercontent.com'
+              render={renderProps => (
+                <button className='authorization' onClick={renderProps.onClick} disabled={renderProps.disabled}>
+                  <img className="settings_entrance" src="./images/google.png" alt="entrance" />
+                </button> 
+              )}
+              buttonText=''
+              onSuccess={this.resposeGoogle}
+              onFailure={this.resposeGoogle}
+              isSignedIn={true}
+              cookiePolicy={'single_host_origin'}
+            />
+
+            <GoogleLogout 
+              clientId='101084119038-ro92b6i4fevieqo57pr9tk136pfsnfo6.apps.googleusercontent.com'
+              buttonText='Выход'
+              onLogoutSuccess={() => {console.log('выход из аккаунта!!!');}}
+            />
+
+            {/* <img className='settings_icon' src='' alt="settings" />
             <button className='authorization'>
               <img className="settings_entrance" src="./images/entrance.png" alt="entrance" />
               <p className='settings_login'>Login</p>
-            </button>
+            </button>  */}
           </div>
         </div>
       </header>
